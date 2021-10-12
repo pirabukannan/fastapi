40cdbfa0a66ca67377a1151e95f0247994ccf8a8
pirabukannan <Apple123m>
2021-10-12 10:50:07 +0200

    values hard coded#

--- main.py
+++ main.py
@@ -10,11 +10,11 @@ import urllib
 
 #DATABASE_URL = "sqlite:///./test.db"
 
-host_server = os.environ.get('host_server', 'localhost')
+host_server = os.environ.get('host_server', 'fastapi-demo-server.postgres.database.azure.com')
 db_server_port = urllib.parse.quote_plus(str(os.environ.get('db_server_port', '5432')))
-database_name = os.environ.get('database_name', 'fastapi')
-db_username = urllib.parse.quote_plus(str(os.environ.get('db_username', 'postgres')))
-db_password = urllib.parse.quote_plus(str(os.environ.get('db_password', 'secret')))
+database_name = os.environ.get('database_name', 'fastapi_db')
+db_username = urllib.parse.quote_plus(str(os.environ.get('db_username', 'fastapi_admin')))
+db_password = urllib.parse.quote_plus(str(os.environ.get('db_password', 'Apple123m')))
 ssl_mode = urllib.parse.quote_plus(str(os.environ.get('ssl_mode','prefer')))
 DATABASE_URL = 'postgresql://{}:{}@{}:{}/{}?sslmode={}'.format(db_username, db_password, host_server, db_server_port, database_name, ssl_mode)
 
